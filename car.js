12fd73e4935a318b23bc80db8272af6df779420b
Abhishek Sharma <ohsharmaabhi03@gmail.com>
2023-01-16 14:37:07 +0530

    add: neural network

--- car.js
+++ car.js
@@ -1,59 +1,63 @@
 class Car{
-    constructor(x, y, width, height, controlType, maxSpeed = 5){
+    constructor(x,y,width,height,controlType,maxSpeed=3){
         this.x = x;
         this.y = y;
-        this.width = width;
         this.height = height;
+        this.width = width;
 
         this.speed = 0;
-        this.acceleration = 0.2;
+        this.acceleration = 3;
         this.maxSpeed = maxSpeed;
-        this.friction = .05;
+        this.friction = 0.05;
         this.angle = 0;
         this.damaged = false;
 
-        this.useBrain = controlType == "AI";
+        this.useBrain = controlType=="AI";
 
-        if(controlType != "DUMMY"){
-            this.sensor = new Sensor(this);
+        this.img = new Image();
+        this.img.src = "redcar.png"
+
+        if(controlType!="DUMMY"){ 
+            this.sensor = new Sensor(this)
             this.brain = new NeuralNetwork(
-                [this.sensor.rayCount, 6, 4]
-            );
+                [this.sensor.rayCount,6,4]
+            )
+            this.img.src = "yellowcar.png";
         }
         this.controls = new Controls(controlType);
+
     }
 
     update(roadBorders, traffic){
         if(!this.damaged){
             this.#move();
-            this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders, traffic);
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders, traffic);
         }
         if(this.sensor){
             this.sensor.update(roadBorders, traffic);
             const offsets = this.sensor.readings.map(
-                s => s == null ? 0 : 1 - s.offset
-            );
+                s=>s==null?0:1-s.offset
+            )
             const outputs = NeuralNetwork.feedForward(offsets, this.brain);
 
             if(this.useBrain){
-                this.controls.forward = outputs[0];
-                this.controls.left = outputs[1];
-                this.controls.right = outputs[2];
-                this.controls.reverse = outputs[3];
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
             }
         }
     }
 
     #assessDamage(roadBorders, traffic){
         for(let i=0; i<roadBorders.length; i++){
-            if(polyIntersect(this.polygon, roadBorders[i])){
+            if(polysIntersect(this.polygon, roadBorders[i])){
                 return true;
             }
         }
-
         for(let i=0; i<traffic.length; i++){
-            if(polyIntersect(this.polygon, traffic[i].polygon)){
+            if(polysIntersect(this.polygon, traffic[i].polygon)){
                 return true;
             }
         }
@@ -61,80 +65,75 @@ class Car{
     }
 
     #createPolygon(){
-        const points = [];
-        const rad = Math.hypot(this.width, this.height) / 2;
+        const points=[];
+        const rad = Math.hypot(this.width, this.height)/2;
         const alpha = Math.atan2(this.width, this.height);
         points.push({
-            x: this.x - Math.sin(this.angle - alpha) * rad,
-            y: this.y - Math.cos(this.angle - alpha) * rad
-        });
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        })
         points.push({
-            x: this.x - Math.sin(this.angle + alpha) * rad,
-            y: this.y - Math.cos(this.angle + alpha) * rad
-        });
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        })
         points.push({
-            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
-            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
-        });
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        })
         points.push({
-            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
-            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
-        });
-        return points
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        })
+        return points;
     }
-
+    
     #move(){
-        if(this.controls.forward){
+        if (this.controls.forward) {
             this.speed += this.acceleration;
         }
-        if(this.controls.reverse){
+        if (this.controls.reverse) {
             this.speed -= this.acceleration;
         }
-        if(this.speed > this.maxSpeed){
+        if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
         }
-        if(this.speed < -this.maxSpeed/2){
-            this.speed = -this.maxSpeed/2;
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
         }
-        if(this.speed > 0){
+        if (this.speed > 0) {
             this.speed -= this.friction;
         }
-        if(this.speed < 0){
+        if (this.speed < 0) {
             this.speed += this.friction;
         }
-        if(Math.abs(this.speed) < this.friction){
-            this.speed=0;
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
         }
-
-        if(this.speed != 0){
+        if (this.speed != 0) {
             const flip = this.speed > 0 ? 1 : -1;
-            if(this.controls.left){
-                this.angle += 0.03*flip;
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
             }
-            if(this.controls.right){
-                this.angle -= 0.03*flip;
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
             }
         }
-
-        this.x-=Math.sin(this.angle)*this.speed;
-        this.y-=Math.cos(this.angle)*this.speed;
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(context, color){
-        if(this.damaged){
-            context.fillStyle = "gray";
-        }else{
-            context.fillStyle = color;
+    draw(ctx,color, drawSensor=false){
+        if(this.sensor && drawSensor){
+            this.sensor.draw(ctx);
         }
-        context.beginPath();
-        context.moveTo(this.polygon[0].x, this.polygon[0].y);
-        for(let i=1; i<this.polygon.length; i++){
-            context.lineTo(this.polygon[i].x, this.polygon[i].y);
-        }
-        context.fill();
-
-        if(this.sensor){
-            this.sensor.draw(context);
-        }
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+        ctx.drawImage(this.img,
+            -this.width/2,
+            -this.height/2,
+            this.width*1.8,
+            this.height);
+        ctx.restore();
     }
 }
--- visualizer.js
+++ visualizer.js
@@ -1,101 +1,105 @@
-class Visualizer{
-    static drawNetwork(context, network){
+class Visualizer {
+    static drawNetwork(ctx, network) {
         const margin = 50;
         const left = margin;
         const top = margin;
-        const width = context.canvas.width - margin * 2;
-        const height = context.canvas.height - margin * 2;
+        const width = ctx.canvas.width - margin * 2;
+        const height = ctx.canvas.height - margin * 2;
 
-        const levelHeight = height/network.levels.length;
+        // Visualizer.drawLevel(ctx, network.levels[0], left, top, width, height); 
 
-        for(let i=network.levels.length-1; i>=0; i--){
+        const levelHeight = height / network.levels.length;
+
+        for (let i = network.levels.length - 1; i >= 0; i--) {
             const levelTop = top + lerp(
                 height - levelHeight,
                 0,
-                network.levels.length == 1 ? 0.5 : i/(network.levels.length - 1)
+                network.levels.length == 1
+                    ? 0.5
+                    : i / (network.levels.length - 1)
             );
-            context.setLineDash([7, 3]);
-            Visualizer.drawLevel(context, network.levels[i],
-                left, levelTop,
-                width, levelHeight,
-                i == network.levels.length - 1 ? ['🠝', '🠜', '🠞', '🠟'] : []
-            )
+
+            ctx.setLineDash([7, 3]);
+            Visualizer.drawLevel(ctx, network.levels[i],
+                left, levelTop, width, levelHeight,
+                i == network.levels.length - 1
+                    ? ['⇧', '⇨', '⇦', '⇩']
+                    : []
+            );
         }
     }
 
-    static drawLevel(context, level, left, top, width, height, outputLabels){
+    static drawLevel(ctx, level, left, top, width, height, outputLabels) {
         const right = left + width;
         const bottom = top + height;
+        const { inputs, outputs, weights, biases } = level;
 
-        const {inputs, outputs, weights, biases} = level;
-        const nodeRadius = 20;
+        for (let i = 0; i < inputs.length; i++) {
+            for (let j = 0; j < outputs.length; j++) {
+                ctx.beginPath();
+                ctx.moveTo(Visualizer.#getNodeX(inputs, i, left, right), bottom);
 
-        for(let i=0; i<inputs.length; i++){
-            for(let j=0; j<outputs.length; j++){
-                context.beginPath();
-                context.moveTo(
-                    Visualizer.#getNodeX(inputs, i, left, right),
-                    bottom
-                );
-                context.lineTo(
-                    Visualizer.#getNodeX(outputs, j, left, right),
-                    top
-                );
-                context.lineWidth = 2;
-                context.strokeStyle = getRGBA(weights[i][j]);
-                context.stroke();
-            }
+                ctx.lineTo(Visualizer.#getNodeX(outputs, j, left, right), top);
+
+                ctx.lineWidth = 2;
+
+                ctx.strokeStyle = getRGBA(weights[i][j]);
+                ctx.stroke();
+            };
         }
 
-        for(let i=0; i<inputs.length; i++){
+        const nodeRadius = 18;
+        for (let i = 0; i < inputs.length; i++) {
             const x = Visualizer.#getNodeX(inputs, i, left, right);
-            context.beginPath();
-            context.arc(x, bottom, nodeRadius, 0, Math.PI*2);
-            context.fillStyle = "black";
-            context.fill();
-            context.beginPath();
-            context.arc(x, bottom, nodeRadius*0.6, 0, Math.PI*2);
-            context.fillStyle = getRGBA(inputs[i]);
-            context.fill();
+            ctx.beginPath();
+            ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2);
+            ctx.fillStyle = "black";
+            ctx.fill();
+            ctx.beginPath();
+            ctx.arc(x, bottom, nodeRadius * 0.6, 0, Math.PI * 2);
+            ctx.fillStyle = getRGBA(inputs[i]);
+            ctx.fill();
         }
 
-        for(let i=0; i<outputs.length; i++){
+        for (let i = 0; i < outputs.length; i++) {
             const x = Visualizer.#getNodeX(outputs, i, left, right);
-            context.beginPath();
-            context.arc(x, top, nodeRadius, 0, Math.PI*2);
-            context.fillStyle = "black";
-            context.fill();
-            context.beginPath();
-            context.arc(x, top, nodeRadius*0.6, 0, Math.PI*2);
-            context.fillStyle = getRGBA(outputs[i]);
-            context.fill();
+            ctx.beginPath();
+            ctx.arc(x, top, nodeRadius, 0, Math.PI * 2);
+            ctx.fillStyle = "black";
+            ctx.fill();
+            ctx.beginPath();
+            ctx.arc(x, top, nodeRadius * 0.6, 0, Math.PI * 2);
+            ctx.fillStyle = getRGBA(outputs[i]);
+            ctx.fill();
 
-            context.beginPath();
-            context.lineWidth = 2;
-            context.arc(x, top, nodeRadius*0.8, 0, Math.PI*2);
-            context.strokeStyle = getRGBA(biases[i]);
-            context.setLineDash([3, 3]);
-            context.stroke();
-            context.setLineDash([]);
+            ctx.beginPath();
+            ctx.arc(x, top, nodeRadius * 0.8, 0, Math.PI * 2);
+            ctx.strokeStyle = getRGBA(biases[i]);
+            ctx.setLineDash([3, 3]);
+            ctx.stroke();
+            ctx.setLineDash([]);
 
-            if(outputLabels[i]){
-                context.beginPath();
-                context.textAlign = "center";
-                context.textBaseline = "middle";
-                context.fillStyle = "black";
-                context.strokeStyle = "white";
-                context.font = (nodeRadius * 1.3) + "px Arial";
-                context.fillText(outputLabels[i], x, top);
-                context.lineWidth = 0.5;
-                context.strokeText(outputLabels[i], x, top);
+            if (outputLabels[i]) {
+                ctx.beginPath();
+                ctx.textAlign = "center";
+                ctx.textBaseline = "middle";
+                ctx.fillStyle = "black";
+                ctx.strokeStyle = "white";
+                ctx.font = (nodeRadius) + "px Arial";
+                ctx.fillText(outputLabels[i], x, top + nodeRadius * 0.1);
+                ctx.lineWidth = 0.5;
+                ctx.strokeText(outputLabels[i], x, top + nodeRadius * 0.1);
             }
         }
+
+
     }
-    static #getNodeX(nodes, index, left, right){
+
+    static #getNodeX(nodes, index, left, right) {
         return lerp(
             left,
             right,
             nodes.length == 1 ? 0.5 : index / (nodes.length - 1)
-        )
+        );
     }
 }
